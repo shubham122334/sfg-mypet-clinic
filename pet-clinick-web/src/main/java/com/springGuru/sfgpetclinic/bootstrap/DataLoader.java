cddb9b6c5a7dd92b91e5c995e525e1317faec642
shubham122334 <shubham1999.iimt@gmail.com>
2023-07-16 03:00:22 +0530

    Added Pet and Contact Info to owners on startup class #31

--- pet-clinick-web/src/main/java/com/springGuru/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinick-web/src/main/java/com/springGuru/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.springGuru.sfgpetclinic.bootstrap;
 
 import com.springGuru.sfgpetclinic.model.Owner;
+import com.springGuru.sfgpetclinic.model.Pet;
 import com.springGuru.sfgpetclinic.model.PetType;
 import com.springGuru.sfgpetclinic.model.Vet;
 import com.springGuru.sfgpetclinic.service.OwnerService;
@@ -8,6 +9,8 @@ import com.springGuru.sfgpetclinic.service.PetTypeService;
 import com.springGuru.sfgpetclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 
 @Component
@@ -43,12 +46,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1=new Owner();
         owner1.setFirstName("shubham");
         owner1.setLastName("prajapati");
+        owner1.setAddress("Dlf phase sec-28");
+        owner1.setCity("Gurgao");
+        owner1.setTelephone("12343122");
+
+        Pet shubhsPet=new Pet();
+        shubhsPet.setPetType(dog);
+        shubhsPet.setOwner(owner1);
+        shubhsPet.setBirthDate(LocalDate.now());
+        shubhsPet.setName("Rasco");
+        owner1.getPets().add(shubhsPet);
 
         ownerService.save(owner1);
 
         Owner owner2=new Owner();
         owner2.setFirstName("Monu");
         owner2.setLastName("Sharma");
+        owner2.setAddress("Noida Sec-16");
+        owner2.setCity("Noida");
+        owner2.setTelephone("21800189");
+
+        Pet monusPet=new Pet();
+        monusPet.setPetType(cat);
+        monusPet.setOwner(owner2);
+        monusPet.setBirthDate(LocalDate.now());
+        monusPet.setName("Suzi");
+        owner2.getPets().add(monusPet);
+
         ownerService.save(owner2);
 
         System.out.println("Owner Loaded.......");
