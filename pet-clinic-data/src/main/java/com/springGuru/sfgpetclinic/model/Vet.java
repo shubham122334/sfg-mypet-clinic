10a038b4779ccefd161af64c60c861004157d6b4
shubham122334 <shubham1999.iimt@gmail.com>
2023-07-22 13:34:54 +0530

    convert vets to jps entity class #37

--- pet-clinic-data/src/main/java/com/springGuru/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springGuru/sfgpetclinic/model/Vet.java
@@ -1,10 +1,17 @@
 package com.springGuru.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+            @JoinTable(name = "vet_specialities",joinColumns = @JoinColumn(name = "vets_id"),
+                        inverseJoinColumns =@JoinColumn(name = "speciality_id"))
     Set<Speciality> specialities=new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
