5815b79f93c5097a4e36977186c78d1bd13098b0
shubham122334 <shubham1999.iimt@gmail.com>
2023-07-23 01:16:49 +0530

    create map impl for visit service closes #45

---
+++ pet-clinic-data/src/main/java/com/springGuru/sfgpetclinic/service/VisitService.java
@@ -0,0 +1,6 @@
+package com.springGuru.sfgpetclinic.service;
+
+import com.springGuru.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springGuru/sfgpetclinic/service/map/VisitServiceMap.java
@@ -0,0 +1,45 @@
+package com.springGuru.sfgpetclinic.service.map;
+
+import com.springGuru.sfgpetclinic.model.Visit;
+import com.springGuru.sfgpetclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findALl();
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+       if(visit.getPet()==null || visit.getPet().getOwner()==null || visit.getPet().getId()==null
+          || visit.getPet().getOwner().getId()==null){
+           throw new RuntimeException("these records are required");
+       }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+
+        super.delete(object);
+    }
+}
