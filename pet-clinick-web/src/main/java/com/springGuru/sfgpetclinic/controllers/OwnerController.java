482d74f83d5547cef7786312c5178ca9d6e28029
shubham122334 <shubham1999.iimt@gmail.com>
2025-11-29 23:46:45 +0530

    Fixed Find By like #1

--- pet-clinick-web/src/main/java/com/springGuru/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinick-web/src/main/java/com/springGuru/sfgpetclinic/controllers/OwnerController.java
@@ -55,7 +55,7 @@ public class OwnerController {
             owner.setLastName("");
         }
 
-        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner> results = ownerService.findAllByLastNameLike("%"+owner.getLastName()+"%");
         System.out.println(owner.getLastName());
         if(results.isEmpty()){
             result.rejectValue("lastName", "notFound", "not found");
